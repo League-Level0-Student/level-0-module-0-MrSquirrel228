e24a91ff4cad0e2bbda2a519d9adb516e1420286
yuandrew2014 <league@WorthyMini-6.attlocal.net>
2018-02-05 16:58:34 -0800

    Module 0 complete. no checkpoint

---
+++ src/Everythingisawesome.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Everythingisawesome {
+public static void main(String[] args) {
+	
+	String name = JOptionPane.showInputDialog("What do you like");
+	JOptionPane.showMessageDialog(null, name + " is awesome");
+	
+}
+}
